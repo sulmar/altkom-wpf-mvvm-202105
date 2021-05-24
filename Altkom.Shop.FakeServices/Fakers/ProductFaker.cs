7d588feca3da667aed5529bd0e415d5776b3e3e1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-05-24 16:17:44 +0200

    ProductFaker

---
+++ Altkom.Shop.FakeServices/Fakers/ProductFaker.cs
@@ -0,0 +1,21 @@
+﻿using Altkom.Shop.Models;
+using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom.Shop.FakeServices.Fakers
+{
+    class ProductFaker : Faker<Product>
+    {
+        public ProductFaker()
+        {
+            string[] colors = new string[] { "red", "blue", "green" };
+
+            RuleFor(p => p.Id, f => f.IndexFaker);
+            RuleFor(p => p.Color, f => f.PickRandom(colors));
+        }
+    }
+}
