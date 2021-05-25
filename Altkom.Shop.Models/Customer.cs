54dfaa58b23c56ad8b2a7b9ea0be62f0d2990256
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-05-25 12:22:05 +0200

    Fody

--- Altkom.Shop.Models/Customer.cs
+++ Altkom.Shop.Models/Customer.cs
@@ -4,30 +4,33 @@ namespace Altkom.Shop.Models
 {
     public class Customer : BaseEntity
     {
-        private string firstName;
-        private string lastName;
+        //private string firstName;
+        //private string lastName;
 
-        public string FirstName
-        {
-            get => firstName;
-            set
-            {
-                firstName = value;
+        //public string FirstName
+        //{
+        //    get => firstName;
+        //    set
+        //    {
+        //        firstName = value;
 
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(FullName));
-            }
-        }
-        public string LastName
-        {
-            get => lastName; set
-            {
-                lastName = value;
+        //        OnPropertyChanged();
+        //        OnPropertyChanged(nameof(FullName));
+        //    }
+        //}
+        //public string LastName
+        //{
+        //    get => lastName; set
+        //    {
+        //        lastName = value;
 
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(FullName));
-            }
-        }
+        //        OnPropertyChanged();
+        //        OnPropertyChanged(nameof(FullName));
+        //    }
+        //}
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string FullName => $"{FirstName} {LastName}";
         public CustomerType CustomerType { get; set; }
         public string Avatar { get; set; }
