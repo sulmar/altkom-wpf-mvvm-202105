369820bbdb97755d192122994b92ffd16d324e8f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-05-26 16:04:52 +0200

    Converter

---
+++ Altkom.Shop.WpfClient/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Altkom.Shop.WpfClient.Converters
+{
+    public class BoolToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool bValue = (bool)value;
+
+            if (targetType!=typeof(Visibility))
+            {
+                throw new InvalidCastException("Oczekiwano Visibility");
+            }
+
+            if (bValue)
+                return Visibility.Collapsed;
+            else
+                return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+    }
+}
