5c54924c2a5bf7373b6025cd1f135406843ae705
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-05-28 16:01:11 +0200

    Walidacje

--- Altkom.Shop.Models/SearchCriterias/ProductSearchCriteria.cs
+++ Altkom.Shop.Models/SearchCriterias/ProductSearchCriteria.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Altkom.Shop.Models.SearchCriterias
@@ -9,7 +11,7 @@ namespace Altkom.Shop.Models.SearchCriterias
 
     }
 
-    public class ProductSearchCriteria : SearchCriteria
+    public class ProductSearchCriteria : SearchCriteria, IDataErrorInfo, INotifyDataErrorInfo
     {
         public string Color { get; set; }
         public decimal? UnitPriceFrom { get; set; }
@@ -17,11 +19,55 @@ namespace Altkom.Shop.Models.SearchCriterias
         public float? WeightFrom { get; set; }
         public float? WeightTo { get; set; }
 
+
+
+
+
+        #region IDataErrorInfo
+
+        // ValidatesOnDataErrors
+        public string Error => string.Empty;
+        public string this[string columnName]
+        {
+            get
+            {
+                if (columnName==nameof(UnitPriceFrom))
+                {
+                    if (UnitPriceFrom < 0)
+                        return $"Poza zakresem";
+                }
+
+                if (columnName == nameof(UnitPriceTo))
+                {
+                    if (UnitPriceTo > 100)
+                        return $"Poza zakresem";
+                }
+
+                return string.Empty;
+               
+            }
+        }
+
+        #endregion
+
         public ProductSearchCriteria()
         {
             UnitPriceFrom = 0;
             UnitPriceTo = 1000;
         }
+
+        #region INotifyDataErrorInfo
+
+        // ValidatesOnNotifyDataErrors
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public bool HasErrors => throw new NotImplementedException();
+        public IEnumerable GetErrors(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 
     public class CustomerSearchCriteria : SearchCriteria
